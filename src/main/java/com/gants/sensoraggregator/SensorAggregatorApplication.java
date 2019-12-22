1523726cab94ba15bef76cbc20d5e68032fa3b14
bgants <bobgants@gmail.com>
2019-12-22 10:11:49 -0500

    Initial project commit.

---
+++ src/main/java/com/gants/sensoraggregator/SensorAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.gants.sensoraggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorAggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gants/sensoraggregator/SensorAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gants.sensoraggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
