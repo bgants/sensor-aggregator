c0b38def24baf996700c661975553568b86b30c7
bgants <bobgants@gmail.com>
2019-12-24 17:27:26 -0500

    Took out in memeory hsql DB and replaced with postgres, changed sensor sensorSampleTime from String to Instant.

--- src/main/java/com/gants/sensoraggregator/model/Sensor.java
+++ src/main/java/com/gants/sensoraggregator/model/Sensor.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.Instant;
 import java.time.LocalDateTime;
 
 @Entity
@@ -18,7 +19,7 @@ public class Sensor {
     private double temperature;
     private double humidity;
     private int sensorGPIONum;
-    private String sensorSampleTime;
+    private Instant sensorSampleTime;
     private String sensorLocation;
     private String sensorName;
 }
